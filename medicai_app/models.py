a9f556c7f95e4dab29ae12b098db601ac17311df
Brian Alexander <brianalexander348@gmail.com>
2025-03-17 20:45:44 +0700

    upload

--- medicai_app/models.py
+++ medicai_app/models.py
@@ -29,3 +29,31 @@ class Hospital(models.Model):
 
     class Meta:
         db_table = 'healthcarefacilities'
+
+class Disease(models.Model):
+    disease_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    precautions = models.TextField()
+
+    class Meta:
+        db_table = 'diseases'
+
+class Doctor(models.Model):
+    doctor_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    specialization = models.CharField(max_length=255)
+    hospital_id = models.IntegerField()
+    contact_info = models.CharField(max_length=255)
+    photo = models.BinaryField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'doctors'
+
+class DoctorSpeciality(models.Model):
+    id = models.AutoField(primary_key=True)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    disease = models.ForeignKey(Disease, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'doctor_specialization'
