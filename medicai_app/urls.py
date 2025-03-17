5a753290c4692d2a75e73df69456012e7eea6f9c
Brian Alexander <brianalexander348@gmail.com>
2025-03-17 19:52:14 +0700

    upload

--- medicai_app/urls.py
+++ medicai_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('symptoms/', get_symptoms, name='get_symptoms'),
     path('register_user/', register_user, name='register_user'),
     path('facilities/', get_hospitals, name='get_facilities'),
-    path('facilities/photo/<int:facility_id>/', get_photo, name='get_facility_photo')
-    path("get_symptom_names/", get_symptom_names, name="get_symptom_names"),
+    path('facilities/photo/<int:facility_id>/', get_photo, name='get_facility_photo'),
+    path('get_symptom_names/', get_symptom_names, name="get_symptom_names"),
 ]
